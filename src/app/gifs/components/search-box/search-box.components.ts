0a720f7cf38486c25dd90c21e02699e1e601a6f4
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-03 12:58:01 -0600

    Validación de servicio

--- src/app/gifs/components/search-box/search-box.components.ts
+++ src/app/gifs/components/search-box/search-box.components.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -17,11 +18,15 @@ export class SearchBoxComponent {
   @ViewChild("txtTagInput")
   public tagInput!: ElementRef<HTMLInputElement>
 
+  constructor ( private gifsService: GifsService) {}
+
  /*  searchTag(newTag: string):void {
     console.log(newTag)
   } */
   searchTag():void {
     const newTag = this.tagInput.nativeElement.value
-    console.log({newTag})
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = "";
   }
 }
