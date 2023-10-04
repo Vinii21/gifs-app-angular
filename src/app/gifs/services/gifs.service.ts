eeb116e52684ee148d03f6d05522ef31e427f362
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-04 09:03:13 -0600

    Colocando tipo de dato a respuesta HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,19 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+const GIPHY_API_KEY = "wkfUxdiDfMjNwPpyKrv1COVkxIatdhnp";
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = [];
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private apiKey: string = GIPHY_API_KEY;
+  private serviceURL: string = "https://api.giphy.com/v1/gifs/"
+
+  constructor(private http: HttpClient ) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -20,9 +28,29 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice( 0, 10);
   }
 
-  searchTag(newTag: string): void {
+  /* async */ searchTag(newTag: string):void/* :Promise<void> */ {
     if(newTag.length === 0) return;
     this.organizeHistory(newTag);
+    /* con promesa, no necesita el async await, pero debe tipar el método con :Promise<void> */
+    /* fetch("https://api.giphy.com/v1/gifs/search?api_key=wkfUxdiDfMjNwPpyKrv1COVkxIatdhnp&q=mario bros&limit=10")
+      .then(resp => resp.json())
+      .then(data => console.log(data)) */
+
+      /* con async await, es decir debe colocar el async al principio del método y el await cuando se este procesando la request */
+      /* const resp = await fetch("https://api.giphy.com/v1/gifs/search?api_key=wkfUxdiDfMjNwPpyKrv1COVkxIatdhnp&q=mario bros&limit=10");
+      const data = await resp.json();
+      console.log(data) */
+
+      /* Con HttpModule de Angular */
+      const params = new HttpParams()
+        .set("api_key", this.apiKey)
+        .set("limit", "10")
+        .set("q", newTag)
+    this.http.get<SearchResponse>(`${this.serviceURL}search`, {params})
+      .subscribe(resp => {
+        this.gifList = resp.data;
+        console.log(this.gifList)
+      })
   }
 
 }
